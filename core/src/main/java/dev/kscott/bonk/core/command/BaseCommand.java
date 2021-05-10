7fa98c3237452aef498de3198e053cc1c0cc3cf7
Kaden Scott <kscottdev@gmail.com>
2021-05-10 10:51:41 -0400

    add BaseCommand

---
+++ core/src/main/java/dev/kscott/bonk/core/command/BaseCommand.java
@@ -0,0 +1,18 @@
+package dev.kscott.bonk.core.command;
+
+import cloud.commandframework.CommandManager;
+import dev.kscott.bonk.api.player.BonkPlayer;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * The base command, extended by all Bonk commands.
+ */
+public abstract class BaseCommand {
+
+    /**
+     * Registers this command with {@code commandManager}.
+     * @param commandManager the Bonk command manager
+     */
+    public abstract void register(final @NonNull CommandManager<BonkPlayer> commandManager);
+
+}
