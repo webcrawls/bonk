a023933f847f981e61f5ed60e3ca6cf442be985b
Kaden Scott <kscottdev@gmail.com>
2021-05-09 20:56:25 -0400

    update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
+import net.kyori.indra.IndraCheckstylePlugin
+import net.kyori.indra.IndraPlugin
+import net.kyori.indra.IndraPublishingPlugin
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-}
+    id("net.kyori.indra") version "1.3.1"
+    id("net.kyori.indra.publishing") version "1.3.1"
+    id("net.kyori.indra.checkstyle") version "1.3.1"
+    id("com.github.johnrengelman.shadow") version "6.1.0"}
 
 group = "dev.kscott.bing"
 version = "1.0.0"
@@ -10,9 +15,27 @@ version = "1.0.0"
 subprojects {
     apply {
         plugin<ShadowPlugin>()
+        plugin<IndraPlugin>()
+        plugin<IndraCheckstylePlugin>()
+        plugin<IndraPublishingPlugin>()
     }
 
     repositories {
         mavenCentral()
+    }
+
+    tasks {
+
+        indra {
+            gpl3OnlyLicense()
+
+            javaVersions {
+                target.set(11)
+            }
+        }
+
+        processResources {
+            expand("version" to rootProject.version)
+        }
     }
 }
