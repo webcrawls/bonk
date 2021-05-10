b0e0a1549d1e08d0b6862f08ada93873fccb5af2
Kaden Scott <kscottdev@gmail.com>
2021-05-10 10:52:20 -0400

    populate BonkPlayer interface

--- api/src/main/java/dev/kscott/bonk/api/player/BonkPlayer.java
+++ api/src/main/java/dev/kscott/bonk/api/player/BonkPlayer.java
@@ -1,4 +1,36 @@
 package dev.kscott.bonk.api.player;
 
-public class BonkPlayer {
+import net.kyori.adventure.audience.ForwardingAudience;
+import net.kyori.adventure.identity.Identified;
+import net.kyori.adventure.identity.Identity;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.dataflow.qual.Pure;
+
+import java.util.UUID;
+
+/**
+ * The base BonkPlayer interface.
+ */
+public interface BonkPlayer extends Identity, Identified, ForwardingAudience.Single {
+
+    /**
+     * {@return the uuid}
+     */
+    @Pure
+    @NonNull UUID uuid();
+
+    /**
+     * {@return the {@link Identity}}
+     */
+    default @NonNull Identity identity() {
+        return this;
+    }
+
+    /**
+     * {@return the username}
+     */
+    @Pure
+    @NonNull String getUsername();
+
+
 }
