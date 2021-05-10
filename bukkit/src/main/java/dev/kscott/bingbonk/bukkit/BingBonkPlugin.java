d5a8aaaeb2e2fd1f7cac8158abd29466f2abdbcb
Kaden Scott <kscottdev@gmail.com>
2021-05-09 22:10:11 -0400

    move to `dev.kscott.bingbonk.bukkit.BingBonk`

--- bukkit/src/main/java/dev/kscott/bingbonk/bukkit/BingBonkPlugin.java
+++ bukkit/src/main/java/dev/kscott/bingbonk/bukkit/BingBonkPlugin.java
@@ -1,8 +1,8 @@
-package dev.kscott.bingbonk;
+package dev.kscott.bingbonk.bukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BingBonk extends JavaPlugin {
+public final class BingBonkPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
