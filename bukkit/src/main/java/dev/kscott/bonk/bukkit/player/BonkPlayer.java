753481e237d90e2d31e52628ab807eabf78b43f4
Kaden Scott <kscottdev@gmail.com>
2021-05-11 15:14:30 -0400

    populate BonkPlayer

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/player/BonkPlayer.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/player/BonkPlayer.java
@@ -1,8 +1,58 @@
 package dev.kscott.bonk.bukkit.player;
 
+import dev.kscott.bonk.bukkit.position.GamePosition;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 /**
  * The core bonk player class.
  */
-public class BonkPlayer {
+public final class BonkPlayer {
+
+    /**
+     * The player associated with this {@code BonkPlayer}.
+     */
+    private final @NonNull Player player;
+
+    /**
+     * Constructs {@code BonkPlayer}.
+     *
+     * @param player the player to associate with this {@code BonkPlayer}
+     */
+    public BonkPlayer(final @NonNull Player player) {
+        this.player = player;
+    }
+
+    /**
+     * {@return the player's position}
+     */
+    public @NonNull GamePosition position() {
+        final @NonNull Location location = this.player.getLocation();
+
+        return new GamePosition(
+                location.getX(),
+                location.getY(),
+                location.getZ(),
+                location.getPitch(),
+                location.getYaw()
+        );
+    }
+
+    /**
+     * Sets the player's position.
+     *
+     * @param position desired position
+     */
+    public void position(final @NonNull GamePosition position) {
+        this.player.teleport(new Location(
+                this.player.getWorld(),
+                position.x(),
+                position.y(),
+                position.z(),
+                position.yaw(),
+                position.pitch()
+        ));
+    }
 
 }
