aa3a01b9ad108b62f996efff63e7810c619d3d90
Kaden Scott <kscottdev@gmail.com>
2021-05-11 07:21:57 -0400

    add CommandMod

---
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/inject/CommandModule.java
@@ -0,0 +1,70 @@
+package dev.kscott.bonk.bukkit.inject;
+
+import cloud.commandframework.CommandManager;
+import cloud.commandframework.bukkit.CloudBukkitCapabilities;
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator;
+import cloud.commandframework.paper.PaperCommandManager;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.function.Function;
+
+/**
+ * Provides the command manager.
+ */
+public class CommandModule extends AbstractModule {
+
+    /**
+     * The command manager.
+     */
+    private final @MonotonicNonNull PaperCommandManager<CommandSender> commandManager;
+
+    /**
+     * Constructs {@code CommandModule}.
+     *
+     * @param plugin the plugin
+     */
+    public CommandModule(final @NonNull Plugin plugin) {
+        try {
+            final @NonNull Function<CommandSender, CommandSender> mapper;
+            mapper = Function.identity();
+
+            commandManager = new PaperCommandManager<>(
+                    plugin,
+                    AsynchronousCommandExecutionCoordinator.<CommandSender>newBuilder().withAsynchronousParsing().build(),
+                    mapper,
+                    mapper
+            );
+
+            if (commandManager.queryCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+                commandManager.registerAsynchronousCompletions();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize the CommandManager");
+        }
+    }
+
+    /**
+     * {@return the command manager}
+     */
+    @Provides
+    @Singleton
+    public CommandManager<CommandSender> provideCommandManager() {
+        return this.commandManager;
+    }
+
+    /**
+     * {@return the paper command manager}
+     */
+    @Provides
+    @Singleton
+    public PaperCommandManager<CommandSender> providePaperCommandManager() {
+        return this.commandManager;
+    }
+
+}
