15fd42d4e29536c7f787bd11ff6cf5b1af96783c
Kaden Scott <kscottdev@gmail.com>
2021-05-12 05:26:58 -0400

    add CommandService

---
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/command/CommandService.java
@@ -0,0 +1,38 @@
+package dev.kscott.bonk.bukkit.command;
+
+import cloud.commandframework.CommandManager;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import dev.kscott.bonk.bukkit.utils.ArrayHelper;
+import org.bukkit.command.CommandSender;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Manages commands.
+ */
+public class CommandService {
+
+    /**
+     * An array of {@link BaseCommand}s to register.
+     */
+    private static final @NonNull Class<? extends BaseCommand>[] COMMANDS = ArrayHelper.create(
+
+    );
+
+    /**
+     * Constructs {@code CommandService}.
+     *
+     * @param injector the Guice injector
+     * @param commandManager the CommandManager
+     */
+    @Inject
+    public CommandService(
+            final @NonNull Injector injector,
+            final @NonNull CommandManager<CommandSender> commandManager
+    ) {
+        for (final @NonNull Class<? extends BaseCommand> klazz : COMMANDS) {
+            injector.getInstance(klazz).register(commandManager);
+        }
+    }
+
+}
