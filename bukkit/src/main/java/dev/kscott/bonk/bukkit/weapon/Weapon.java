1c9833c797961454c83a38895e325b055cdf72b6
Kaden Scott <kscottdev@gmail.com>
2021-05-12 05:46:32 -0400

    turn Weapon into a record

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/weapon/Weapon.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/weapon/Weapon.java
@@ -14,93 +14,14 @@ import java.util.List;
 /**
  * Represents a playable Bonk weapon.
  */
-public class Weapon {
-
-    /**
-     * The id of this weapon.
-     */
-    private final @NonNull String id;
-
-    /**
-     * The name of the weapon.
-     */
-    private final @NonNull Component name;
-
-    /**
-     * The description of the weapon.
-     */
-    private final @NonNull List<Component> description;
-
-    /**
-     * The material of the weapon.
-     */
-    private final @NonNull Material material;
-
-    /**
-     * The key of the weapon, for use in PDC operations.
-     */
-    private final @NonNull NamespacedKey weaponKey;
-
-    /**
-     * The sounds of the weapon.
-     */
-    private final @NonNull List<WeaponSoundDefinition> sounds;
-
-    /**
-     * Constructs Weapon.
-     *
-     * @param id          id
-     * @param name        name
-     * @param description description
-     * @param material    material
-     * @param sounds      sounds to play when used
-     * @param weaponKey   namespaced key
-     */
-    public Weapon(
-            final @NonNull String id,
-            final @NonNull Component name,
-            final @NonNull List<Component> description,
-            final @NonNull Material material,
-            final @NonNull List<WeaponSoundDefinition> sounds,
-            final @NonNull NamespacedKey weaponKey
-    ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.material = material;
-        this.weaponKey = weaponKey;
-        this.sounds = sounds;
-    }
-
-
-    /**
-     * {@return the id}
-     */
-    public @NonNull String id() {
-        return id;
-    }
-
-    /**
-     * {@return the name}
-     */
-    public @NonNull Component name() {
-        return name;
-    }
-
-    /**
-     * {@return the description}
-     */
-    public @NonNull List<Component> description() {
-        return Collections.unmodifiableList(this.description);
-    }
-
-    /**
-     * {@return the weapon sounds}
-     */
-    public @NonNull List<WeaponSoundDefinition> sounds() {
-        return Collections.unmodifiableList(this.sounds);
-    }
-
+public record Weapon(
+        @NonNull String id,
+        @NonNull Component name,
+        @NonNull List<Component> description,
+        @NonNull Material material,
+        @NonNull List<WeaponSoundDefinition> sounds,
+        @NonNull NamespacedKey key
+) {
     /**
      * {@return the ItemStack of this weapon}
      */
@@ -108,7 +29,105 @@ public class Weapon {
         return AdventureItemBuilder.adventure(this.material)
                 .name(this.name)
                 .loreComponents(this.description)
-                .data(this.weaponKey, PersistentDataType.STRING, this.id)
+                .data(this.key, PersistentDataType.STRING, this.id)
                 .build();
     }
+
 }
+
+//    /**
+//     * The id of this weapon.
+//     */
+//    private final @NonNull String id;
+//
+//    /**
+//     * The name of the weapon.
+//     */
+//    private final @NonNull Component name;
+//
+//    /**
+//     * The description of the weapon.
+//     */
+//    private final @NonNull List<Component> description;
+//
+//    /**
+//     * The material of the weapon.
+//     */
+//    private final @NonNull Material material;
+//
+//    /**
+//     * The key of the weapon, for use in PDC operations.
+//     */
+//    private final @NonNull NamespacedKey weaponKey;
+//
+//    /**
+//     * The sounds of the weapon.
+//     */
+//    private final @NonNull List<WeaponSoundDefinition> sounds;
+//
+//    /**
+//     * Constructs Weapon.
+//     *
+//     * @param id          id
+//     * @param name        name
+//     * @param description description
+//     * @param material    material
+//     * @param sounds      sounds to play when used
+//     * @param weaponKey   namespaced key
+//     */
+//    public Weapon(
+//            final @NonNull String id,
+//            final @NonNull Component name,
+//            final @NonNull List<Component> description,
+//            final @NonNull Material material,
+//            final @NonNull List<WeaponSoundDefinition> sounds,
+//            final @NonNull NamespacedKey weaponKey
+//    ) {
+//        this.id = id;
+//        this.name = name;
+//        this.description = description;
+//        this.material = material;
+//        this.weaponKey = weaponKey;
+//        this.sounds = sounds;
+//    }
+//
+//
+//    /**
+//     * {@return the id}
+//     */
+//    public @NonNull String id() {
+//        return id;
+//    }
+//
+//    /**
+//     * {@return the name}
+//     */
+//    public @NonNull Component name() {
+//        return name;
+//    }
+//
+//    /**
+//     * {@return the description}
+//     */
+//    public @NonNull List<Component> description() {
+//        return Collections.unmodifiableList(this.description);
+//    }
+//
+//    /**
+//     * {@return the weapon sounds}
+//     */
+//    public @NonNull List<WeaponSoundDefinition> sounds() {
+//        return Collections.unmodifiableList(this.sounds);
+//    }
+//
+//    /**
+//     * {@return the ItemStack of this weapon}
+//     */
+//    public @NonNull ItemStack itemStack() {
+//        return AdventureItemBuilder.adventure(this.material)
+//                .name(this.name)
+//                .loreComponents(this.description)
+//                .data(this.weaponKey, PersistentDataType.STRING, this.id)
+//                .build();
+//    }
+//}
