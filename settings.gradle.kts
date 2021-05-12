4af30893aa65799c88c78070528e063b6fc2e217
Kaden Scott <kscottdev@gmail.com>
2021-05-12 01:27:16 -0400

    remove core/api project definitions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
 rootProject.name = "bonk"
 
-include("core", "bukkit", "api")
-project(":core").name = "bonk-core"
+include("bukkit")
 project(":bukkit").name = "bonk-bukkit"
-project(":api").name = "bonk-api"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("VERSION_CATALOGS")
