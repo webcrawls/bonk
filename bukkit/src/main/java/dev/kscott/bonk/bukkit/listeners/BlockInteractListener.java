3ef89dac28fe5d25a21c484629a64895e0f74170
Kaden Scott <kscottdev@gmail.com>
2021-05-12 08:31:37 -0400

    finish BlockInteractListener

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/BlockInteractListener.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/BlockInteractListener.java
@@ -2,17 +2,22 @@ package dev.kscott.bonk.bukkit.listeners;
 
 import com.google.inject.Inject;
 import dev.kscott.bonk.bukkit.block.BlockService;
+import dev.kscott.bonk.bukkit.entity.EntityService;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Random;
 
 /**
  * Handles interactions with blocks.
@@ -25,9 +30,19 @@ public class BlockInteractListener implements Listener {
     private final @NonNull BlockService blockService;
 
     /**
+     * The entity service.
+     */
+    private final @NonNull EntityService entityService;
+
+    /**
      * The plugin.
      */
     private final @NonNull JavaPlugin plugin;
+
+    /**
+     * Random instance.
+     */
+    private final @NonNull Random random;
 
     /**
      * Constructs {@code BlockInteractListener}.
@@ -38,10 +53,13 @@ public class BlockInteractListener implements Listener {
     @Inject
     public BlockInteractListener(
             final @NonNull JavaPlugin plugin,
-            final @NonNull BlockService blockService
+            final @NonNull BlockService blockService,
+            final @NonNull EntityService entityService
     ) {
         this.plugin = plugin;
+        this.random = new Random();
         this.blockService = blockService;
+        this.entityService = entityService;
     }
 
     /**
@@ -63,11 +81,29 @@ public class BlockInteractListener implements Listener {
 
         event.setCancelled(true);
 
-        final @NonNull BlockData blockData = block.getBlockData().clone();
-        final @NonNull Directional directionalBlockData = (Directional) blockData;
-        final @NonNull BlockFace face = directionalBlockData.getFacing();
+        final @NonNull BlockState state = block.getState(true);
+
+        final @NonNull BlockFace pumpkinFace = ((Directional) block.getBlockData()).getFacing();
 
         block.setType(Material.CARVED_PUMPKIN);
+
+        final @NonNull Directional pumpkinBlockData = ((Directional) block.getBlockData());
+        pumpkinBlockData.setFacing(pumpkinFace);
+
+        block.setBlockData(pumpkinBlockData);
+
+        // Regen between 2-4 seconds
+        final int regenTime = random.nextInt(80) + 40;
+
+        this.entityService.launchEntities(block.getLocation(), 4, 2, true);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                state.update(true);
+            }
+        }.runTaskLater(this.plugin, regenTime);
+
     }
 
 }
