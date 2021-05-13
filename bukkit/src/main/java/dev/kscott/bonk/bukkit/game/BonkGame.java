ac393685ab5864aff358442779c25b90cf59a23e
Kaden Scott <kscottdev@gmail.com>
2021-05-13 11:33:00 -0400

    add  BonkGame#enable

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/game/BonkGame.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/game/BonkGame.java
@@ -5,9 +5,15 @@ import com.google.inject.Injector;
 import dev.kscott.bonk.bukkit.command.CommandService;
 import dev.kscott.bonk.bukkit.inject.CommandModule;
 import dev.kscott.bonk.bukkit.inject.GameModule;
+import dev.kscott.bonk.bukkit.log.LoggingService;
+import dev.kscott.bonk.bukkit.minigame.CreeperMinigame;
+import dev.kscott.bonk.bukkit.minigame.Minigame;
+import dev.kscott.bonk.bukkit.utils.ArrayHelper;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
 
 /**
  * Manages the Bonk game.
@@ -15,33 +21,51 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 public final class BonkGame {
 
     /**
+     * The array of minigames to enable.
+     */
+    private static final @NonNull Class<? extends Minigame>[] MINIGAMES = ArrayHelper.create(
+            CreeperMinigame.class
+    );
+
+    /**
      * The injector provided by the parent class
      */
     private final @NonNull Injector parentInjector;
+    /**
+     * The plugin.
+     */
+    private final @NonNull JavaPlugin plugin;
+    /**
+     * The pogging service.
+     */
+    private final @NonNull LoggingService loggingService;
+    /**
+     * A list of enabled minigames.
+     */
+    private final @NonNull List<Minigame> enabledMinigames;
 
     /**
      * The injector used by {@code BonkGame}.
      */
     private @MonotonicNonNull Injector injector;
 
-    /**
-     * The plugin.
-     */
-    private final @NonNull JavaPlugin plugin;
 
     /**
      * Constructs {@code BonkGame}.
      *
      * @param parentInjector the parent injector
      * @param plugin         the plugin
+     * @param loggingService         the logging service
      */
     @Inject
     public BonkGame(
             final @NonNull Injector parentInjector,
-            final @NonNull JavaPlugin plugin
+            final @NonNull JavaPlugin plugin,
+            final @NonNull LoggingService loggingService
     ) {
         this.parentInjector = parentInjector;
         this.plugin = plugin;
+        this.loggingService = loggingService;
     }
 
     /**
@@ -60,6 +84,17 @@ public final class BonkGame {
         this.injector.getInstance(CommandService.class);
 
         return this.injector;
+    }
+
+    /**
+     * Enables the Bonk game.
+     */
+    public void enable() {
+        // load minigames
+        for (final @NonNull Class<? extends Minigame> klazz : MINIGAMES) {
+            final @NonNull Minigame minigame = this.injector.getInstance(klazz);
+            this.loggingService.debug("Enabled minigame "+minigame.getClass().getSimpleName());
+        }
     }
 
 }
