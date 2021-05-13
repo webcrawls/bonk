17b213dd2616d5f4a89ba4bd938ac256c8706a5a
Kaden Scott <kscottdev@gmail.com>
2021-05-13 05:35:10 -0400

    reset player when fallen below y=1

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/PlayerMovementListeners.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/PlayerMovementListeners.java
@@ -1,7 +1,10 @@
 package dev.kscott.bonk.bukkit.listeners;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import com.google.common.cache.LoadingCache;
 import com.google.inject.Inject;
+import dev.kscott.bonk.bukkit.player.PlayerService;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,13 +25,39 @@ public class PlayerMovementListeners implements Listener {
     private final @NonNull JavaPlugin plugin;
 
     /**
+     * The player service.
+     */
+    private final @NonNull PlayerService playerService;
+
+    /**
      * Constructs {@link PlayerMovementListeners}.
      *
      * @param plugin the plugin
+     * @param playerService the player service
      */
     @Inject
-    public PlayerMovementListeners(final @NonNull JavaPlugin plugin) {
+    public PlayerMovementListeners(
+            final @NonNull JavaPlugin plugin,
+            final @NonNull PlayerService playerService
+    ) {
         this.plugin = plugin;
+        this.playerService = playerService;
+    }
+
+    /**
+     * Processes all {@link PlayerMoveEvent}-related data.
+     */
+    @EventHandler
+    public void playerMoveEvent(final @NonNull PlayerMoveEvent event) {
+        final @NonNull Player player = event.getPlayer();
+        final @NonNull Location from = event.getFrom();
+
+        // Reset player if y >= 0
+        if (from.getBlockY() <= 1) {
+            this.playerService.reset(player);
+            return;
+        }
+
     }
 
 }
