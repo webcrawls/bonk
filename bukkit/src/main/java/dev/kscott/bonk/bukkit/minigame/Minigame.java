19ed30d32aaf2ff7f2d647c723be4ef3f39ad0c0
Kaden Scott <kscottdev@gmail.com>
2021-05-13 02:14:27 -0400

    add Minigame interface

---
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/minigame/Minigame.java
@@ -0,0 +1,14 @@
+package dev.kscott.bonk.bukkit.minigame;
+
+/**
+ * Represents a Minigame.
+ */
+public interface Minigame {
+
+    void enable();
+
+    void disable();
+
+    boolean enabled();
+
+}
