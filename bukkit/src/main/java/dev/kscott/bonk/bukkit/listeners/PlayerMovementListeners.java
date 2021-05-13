47756f06cddd741c4ef7e784a130fcbe6f3a27bc
Kaden Scott <kscottdev@gmail.com>
2021-05-13 05:05:00 -0400

    remove player jump listener

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/PlayerMovementListeners.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/PlayerMovementListeners.java
@@ -5,6 +5,7 @@ import com.google.inject.Inject;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -28,33 +29,6 @@ public class PlayerMovementListeners implements Listener {
     @Inject
     public PlayerMovementListeners(final @NonNull JavaPlugin plugin) {
         this.plugin = plugin;
-    }
-
-    /**
-     * Makes player jumps a bit higher.
-     *
-     * @param event {@link PlayerJumpEvent}
-     */
-    @EventHandler
-    public void handlePlayerJump(final @NonNull PlayerJumpEvent event) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                final @NonNull Player player = event.getPlayer();
-
-                final @NonNull Vector jumpVelocity = player.getVelocity();
-
-                System.out.println(jumpVelocity);
-
-                jumpVelocity.multiply(new Vector(1, 1.25, 1));
-
-                System.out.println("test");
-
-                System.out.println(jumpVelocity);
-
-                player.setVelocity(jumpVelocity);
-            }
-        }.runTaskLater(this.plugin, 1);
     }
 
 }
