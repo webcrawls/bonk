d2d99aa8fcf347ecc1e7c66f8923c1d4ea4cb921
Kaden Scott <kscottdev@gmail.com>
2021-05-14 14:39:06 -0400

    write some chat code

--- bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/ChatListener.java
+++ bukkit/src/main/java/dev/kscott/bonk/bukkit/listeners/ChatListener.java
@@ -1,4 +1,39 @@
 package dev.kscott.bonk.bukkit.listeners;
 
-public class ChatListener {
+import com.google.inject.Inject;
+import dev.kscott.bonk.bukkit.chat.ChatService;
+import dev.kscott.bonk.bukkit.player.PlayerService;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Listens on chat event.
+ */
+public class ChatListener implements Listener {
+
+    /**
+     * The chat service.
+     */
+    private final @NonNull ChatService chatService;
+
+    /**
+     * The player service.
+     */
+    private final @NonNull PlayerService playerService;
+
+    @Inject
+    public ChatListener(
+            final @NonNull ChatService chatService,
+            final @NonNull PlayerService playerService
+    ) {
+        this.chatService = chatService;
+        this.playerService = playerService;
+    }
+
+    @EventHandler
+    public void chat(final @NonNull AsyncChatEvent event) {
+    }
+
 }
